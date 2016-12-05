ba9d4e260d648931b6af734222a5ec50657598bb
Sven-Siyuan-Wang <siyuanwang1123@gmail.com>
2016-12-05 22:24:43 +0800

    added ES globalPolarWords. EN: f=0.2818; ES: f= 0.2235

---
+++ finalEval.sh
@@ -0,0 +1,6 @@
+echo "=============== PART 5 =============="
+echo "=============== EN =============="
+python evalResult.py EN/dev.out EN/dev.p5.out
+echo "=============== ES =============="
+python evalResult.py ES/dev.out ES/dev.p5.out
+
